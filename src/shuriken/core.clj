87ef593158e0149a3e1e102ef07c05e9ab2ae941
unexpectedness <simplertodescribethantogenerate@proton.me>
2016-11-20 17:26:47 +0100

    Added fully-qualify

--- src/shuriken/core.clj
+++ src/shuriken/core.clj
@@ -19,3 +19,17 @@
           false)))))
 
 (def not? complement)
+
+(defn fully-qualify
+  ([sym]
+   (fully-qualify *ns* sym))
+  ([ns sym]
+    (if-let [r (ns-resolve *ns* sym)]
+      (if (class? r)
+        (-> r str
+            (clojure.string/replace #"(?:class|interface) " "")
+            symbol)
+        (-> r meta :ns str
+            (str "/" (.sym r))
+            symbol))
+      sym)))
