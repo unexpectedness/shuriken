b93b8afd2a216643fca20a937fa135270ba6b2b5
unexpectedness <simplertodescribethantogenerate@proton.me>
2016-11-20 17:27:03 +0100

    Added test for fully-qualify

---
+++ test/shuriken/core_test.clj
@@ -0,0 +1,16 @@
+(ns shuriken.core-test
+  (:require [clojure.test :refer :all]
+            [shuriken.core :refer [fully-qualify]]
+            [shuriken.virtual-test-namespace :refer [a-var] :as virtual])
+  (:import [shuriken.virtual_test_namespace AType]))
+
+(deftest test-fully-qualify
+  (let [local-var :local]
+    (binding [*ns* (find-ns 'shuriken.core-test)]
+      (are [sym expected] (= (fully-qualify *ns* sym) expected)
+           'Object              'java.lang.Object
+           'fully-qualify       'shuriken.core/fully-qualify
+           'local-var           'local-var
+           'a-var               'shuriken.virtual-test-namespace/a-var
+           'virtual/another-var 'shuriken.virtual-test-namespace/another-var
+           'AType               'shuriken.virtual_test_namespace.AType))))
---
+++ test/shuriken/virtual_test_namespace.clj
@@ -0,0 +1,5 @@
+(ns shuriken.virtual-test-namespace)
+
+(def a-var        :a-var)
+(def another-var  :another-var)
+(deftype AType [])
