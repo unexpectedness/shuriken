11510b6cd57947181a0d796e25cb344567d560fe
unexpectedness <simplertodescribethantogenerate@proton.me>
2017-01-16 23:14:23 +0100

    Bump version to 0.7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clojars.unexpectedness/shuriken "0.6.0"
+(defproject net.clojars.unexpectedness/shuriken "0.7.0"
   :description "unexpectedness' Clojure toolbox"
   :url "https://github.com/unexpectedness/shuriken"
   :license {:name "Eclipse Public License"
