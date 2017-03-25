fded00236f75847e688f24cdb4b833b6df1a835b
unexpectedness <simplertodescribethantogenerate@proton.me>
2017-03-25 14:30:51 +0100

    Fix macroexpand-do

--- src/shuriken/macro.clj
+++ src/shuriken/macro.clj
@@ -1,15 +1,25 @@
 (ns shuriken.macro)
 
+(defn dequote [expr]
+  (if (and (list? expr)
+           (= 'quote (first expr)))
+    (second expr)
+    expr))
+
 (defmacro macroexpand-do
+  ([expr]
+   `(macroexpand-do nil ~expr))
   ([mode expr]
    (let [macro-sym (case mode
-                     0    'macroexpand
+                     nil  'macroexpand
                      1    'macroexpand-1
                      :all 'clojure.walk/macroexpand-all)]
      `(do (println "-- Macro expansion -- ")
-          (clojure.pprint/pprint
-            (~macro-sym ~expr))
+          (clojure.pprint/pprint (~macro-sym ~expr))
           (newline)
+          
           (println "-- Running macro --")
-          ~expr
-          (newline)))))
+          (let [result# ~(dequote expr)]
+            (clojure.pprint/pprint result#)
+            (newline)
+            result#)))))
