ec5a3f8373be471ef8364294d6c5e6c59c178cd1
unexpectedness <simplertodescribethantogenerate@proton.me>
2017-03-25 20:38:53 +0100

    Bump version to 0.12.0

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Small yet effective Clojure weapons.
 # Usage
 
 ```clojure
-[net.clojars.unexpectedness/shuriken "0.11.0"]
+[net.clojars.unexpectedness/shuriken "0.12.0"]
 ```
 
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clojars.unexpectedness/shuriken "0.11.0"
+(defproject net.clojars.unexpectedness/shuriken "0.12.0"
   :description "unexpectedness' Clojure toolbox"
   :url "https://github.com/unexpectedness/shuriken"
   :license {:name "Eclipse Public License"
